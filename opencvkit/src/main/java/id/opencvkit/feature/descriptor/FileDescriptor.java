8dfb3f4d9808375c531fda6f0d8341c4b7e7b873
lambdaprime <intid@protonmail.com>
2021-08-19 01:57:05 +0000

    Adding feature related classes

---
+++ opencvkit/src/main/java/id/opencvkit/feature/descriptor/FileDescriptor.java
@@ -0,0 +1,23 @@
+package id.opencvkit.feature.descriptor;
+
+import java.nio.file.Path;
+
+import org.opencv.core.Mat;
+
+/**
+ * Extends Mat with additional file information which this Mat
+ * descriptor describes.
+ */
+public class FileDescriptor extends Mat {
+
+    private Path file;
+
+    public FileDescriptor(Mat descriptor, Path file) {
+        descriptor.assignTo(this);
+        this.file = file;
+    }
+    
+    public Path getFile() {
+        return file;
+    }
+}
---
+++ opencvkit/src/main/java/id/opencvkit/feature/match/MatchResult.java
@@ -0,0 +1,32 @@
+package id.opencvkit.feature.match;
+
+public class MatchResult<T> {
+
+    private T a, b;
+    private float distance;
+    
+    public MatchResult(T a, T b, float distance) {
+        this.a = a;
+        this.b = b;
+        this.distance = distance;
+    }
+    
+    public float getDistance() {
+        return distance;
+    }
+    
+    public T getA() {
+        return a;
+    }
+    
+    public T getB() {
+        return b;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("distance=%s, a='%s', b='%s'", distance,
+                a, b);
+    }
+
+}
